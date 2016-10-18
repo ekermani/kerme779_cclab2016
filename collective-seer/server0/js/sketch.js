dbd3d7347012c69ea24e3e56f905c4d71f073b7f
Erica Kermani <ekermani@gmail.com>
2016-10-17 20:28:53 -0400

    getting drag/drop images to work

--- collective-seer/server0/js/sketch.js
+++ collective-seer/server0/js/sketch.js
@@ -1,26 +1,69 @@
-// adapted from Daniel Shiffman's p5.js Muybridge style animation tutorial video 
+// Video Capture: adapted from Daniel Shiffman's p5.js Muybridge style animation tutorial video 
+// Face Detection: https://kylemcdonald.github.io/cv-examples/FaceDetection/sketch.js
+// Shiffman/Coding Rainbow: Drag and Drop tutorial
 
 var video;
 var button;
 var record;
+var dropzone;
+// var tracker;
 // var snapshots = [];
 // var counter = 0;
 // var total = 43;
 
 function setup() {
-    createCanvas(640, 480);
-    background(51);
+    var c = createCanvas(640, 480);
+    // devicePixelScaling(false);
+    background(0);
+    c.drop(gotFile);
     // capture = createCapture(VIDEO, ready);
-    video = createCapture(VIDEO);
+    // video = createCapture(VIDEO);
 
-    video.size(640, 480);
+    // video.size(640, 480);
     //capture.hide();
-    button = createButton('System Ready');
-    button.mousePressed(takesnap);
-    record = createButton('Save Image');
-    record.mousePressed(recorded);
+    // button = createButton('System Ready');
+    // button.mousePressed(takesnap);
+    // record = createButton('Save Image');
+    // record.mousePressed(recorded);
+
+    // dropzone = select('#dropzone');
+    // dropzone.dragOver();
+
+    // tracker = new clm.tracker();
+    // tracker = init(pModel);
+    // tracker.start(video.elt);
 
 }
+
+function draw() {
+    fill(255);
+    noStroke();
+    noLoop();
+
+}
+
+function gotFile(file) {
+    if(file.type === 'image'){
+        var img = createImg(file.data).hide();
+        image(img, 0, 0, width, height);
+    } else {
+        println('Not an image');
+
+    }
+}
+
+
+//     var img = createImg(file.data).hide();
+//     image(img, 0, 0, width, height);
+//     console.log(img);
+//     noStroke();
+//     for (var i = 0; i < width; i+=10) {
+//         for (var j=0; j < height; j+=10) {
+//             fill(get(i, j));
+//             ellipse(i, j, 10, 10);
+//         }
+//     }
+// }
 
 // var go = false; 
 
@@ -28,17 +71,42 @@ function setup() {
 // 	go = true;
 // }
 
-function takesnap() {
-    image(video,0,0);
-}
+// function highlight() {
+//     dropzone.style('background-color', '#ccc');
+// }
 
-function recorded() {
-// saveFrames(filename,extension,_duration,_fps,callback)
-    saveFrames("image-","png", 3, 30);
-}
+// function takesnap() {
+//     image(video,0,0);
+// // //     var positions = tracker.getCurrentPosition();
+// }
 
-function draw() {
-    tint(255, 50);
+// function recorded() {
+// // saveFrames(filename,extension,_duration,_fps,callback)
+//     // saveFrames("image-","png", 1, 1);
+//     // saveFrames("image-","png");
+//     save(0);
+
+// }
+
+
+    // tint(255, 50);
+    // image(video,0,0);
+    // var positions = tracker.getCurrentPosition();
+
+    // noFill();
+    // stroke(255);
+    // beginShape();
+    // for (var i = 0; i < positions.length; i++) {
+    //     fill(map(i, 0, positions.length, 0, 360), 50, 100);
+    //     ellipse(positions[i][0], positions[i][1], 4, 4);
+    //     text(i, positions[i],[0], positions[i][1]);
+    // }
+
+    // if(positions.length > 0) {
+    //     var mouthLeft = createVector(positions[44][0], positions[44][1]);
+    //     var mouthRight = createVector(positions[50][0], positions[50][1]);
+    //     var smile = mouthLeft.dist(mouthRight);
+
     // image(video,0,0);
 
     // snapshots.push(video.get());
@@ -64,7 +132,6 @@ function draw() {
   // 	  	counter = 0;
 
   // 	  }
-}
 
 //   var w = 80;
 //   var h = 60;
